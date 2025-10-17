e90dce9c21ef9eba54e1096e0f2f28eb1046a0c6
omartmam <omartmam9@gmail.com>
2025-10-17 15:54:04 +0300

    add all assets

--- lib/Core/utils/custom_adaptive_layout.dart
+++ lib/Core/utils/custom_adaptive_layout.dart
@@ -4,21 +4,15 @@ import 'package:open_fashion/Core/utils/size_config.dart';
 class CustomAdaptiveLayout extends StatelessWidget {
   final WidgetBuilder mobileLayout, tabletLayout;
 
-  const CustomAdaptiveLayout({
-    super.key,
-    required this.mobileLayout,
-    required this.tabletLayout,
-  });
+  const CustomAdaptiveLayout({super.key, required this.mobileLayout, required this.tabletLayout});
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth < SizeConfig.tabletBreakPoint) {
-          return mobileLayout(context);
-        } else {
-          return tabletLayout(context);
-        }
+        return constraints.maxWidth < SizeConfig.tabletBreakPoint
+            ? mobileLayout(context)
+            : tabletLayout(context);
       },
     );
   }
