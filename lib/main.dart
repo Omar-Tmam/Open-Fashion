bc73ed7fa490fa10ea168b4c84993576c0eaeb0a
omartmam <omartmam9@gmail.com>
2025-10-17 15:16:42 +0300

    init repo

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const OpenFashion());
+}
+
+class OpenFashion extends StatelessWidget {
+  const OpenFashion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
