0a5eb0764fa68188056fce37726327f4f7bee2bc
omartmam <omartmam9@gmail.com>
2025-10-17 15:57:56 +0300

    generate assets file and add font

---
+++ lib/Core/assets.dart
@@ -0,0 +1,189 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imgsArrow
+  /// assets/imgs/Arrow.svg
+  static const String imgsArrow = "assets/imgs/Arrow.svg";
+
+  /// Assets for imgsArrow1
+  /// assets/imgs/Arrow-1.svg
+  static const String imgsArrow1 = "assets/imgs/Arrow-1.svg";
+
+  /// Assets for imgsBack
+  /// assets/imgs/Back.svg
+  static const String imgsBack = "assets/imgs/Back.svg";
+
+  /// Assets for imgsBackward
+  /// assets/imgs/backward.svg
+  static const String imgsBackward = "assets/imgs/backward.svg";
+
+  /// Assets for imgsBoots
+  /// assets/imgs/boots.png
+  static const String imgsBoots = "assets/imgs/boots.png";
+
+  /// Assets for imgsBracelet
+  /// assets/imgs/Bracelet.png
+  static const String imgsBracelet = "assets/imgs/Bracelet.png";
+
+  /// Assets for imgsBracelets
+  /// assets/imgs/Bracelets.png
+  static const String imgsBracelets = "assets/imgs/Bracelets.png";
+
+  /// Assets for imgsCall
+  /// assets/imgs/Call.svg
+  static const String imgsCall = "assets/imgs/Call.svg";
+
+  /// Assets for imgsClose
+  /// assets/imgs/Close.svg
+  static const String imgsClose = "assets/imgs/Close.svg";
+
+  /// Assets for imgsDo Not Bleach
+  /// assets/imgs/Do Not Bleach.svg
+  static const String imgsDoNotBleach = "assets/imgs/Do Not Bleach.svg";
+
+  /// Assets for imgsDo Not Tumble Dry
+  /// assets/imgs/Do Not Tumble Dry.svg
+  static const String imgsDoNotTumbleDry = "assets/imgs/Do Not Tumble Dry.svg";
+
+  /// Assets for imgsDo Not Wash
+  /// assets/imgs/Do Not Wash.svg
+  static const String imgsDoNotWash = "assets/imgs/Do Not Wash.svg";
+
+  /// Assets for imgsDoor to Door Delivery
+  /// assets/imgs/Door to Door Delivery.svg
+  static const String imgsDoortoDoorDelivery = "assets/imgs/Door to Door Delivery.svg";
+
+  /// Assets for imgsDown
+  /// assets/imgs/Down.svg
+  static const String imgsDown = "assets/imgs/Down.svg";
+
+  /// Assets for imgsDress
+  /// assets/imgs/dress.png
+  static const String imgsDress = "assets/imgs/dress.png";
+
+  /// Assets for imgsEarRing
+  /// assets/imgs/ear_ring.png
+  static const String imgsEarRing = "assets/imgs/ear_ring.png";
+
+  /// Assets for imgsEllipse 61
+  /// assets/imgs/Ellipse 61.svg
+  static const String imgsEllipse61 = "assets/imgs/Ellipse 61.svg";
+
+  /// Assets for imgsEllipse 62
+  /// assets/imgs/Ellipse 62.svg
+  static const String imgsEllipse62 = "assets/imgs/Ellipse 62.svg";
+
+  /// Assets for imgsExport
+  /// assets/imgs/Export.svg
+  static const String imgsExport = "assets/imgs/Export.svg";
+
+  /// Assets for imgsFilter
+  /// assets/imgs/Filter.svg
+  static const String imgsFilter = "assets/imgs/Filter.svg";
+
+  /// Assets for imgsForward
+  /// assets/imgs/Forward.svg
+  static const String imgsForward = "assets/imgs/Forward.svg";
+
+  /// Assets for imgsForward Arrow
+  /// assets/imgs/Forward Arrow.svg
+  static const String imgsForwardArrow = "assets/imgs/Forward Arrow.svg";
+
+  /// Assets for imgsGallery
+  /// assets/imgs/Gallery.svg
+  static const String imgsGallery = "assets/imgs/Gallery.svg";
+
+  /// Assets for imgsGroup 198
+  /// assets/imgs/Group 198.svg
+  static const String imgsGroup198 = "assets/imgs/Group 198.svg";
+
+  /// Assets for imgsHeart
+  /// assets/imgs/Heart.svg
+  static const String imgsHeart = "assets/imgs/Heart.svg";
+
+  /// Assets for imgsInstagram
+  /// assets/imgs/Instagram.svg
+  static const String imgsInstagram = "assets/imgs/Instagram.svg";
+
+  /// Assets for imgsIron Low Temperature
+  /// assets/imgs/Iron Low Temperature.svg
+  static const String imgsIronLowTemperature = "assets/imgs/Iron Low Temperature.svg";
+
+  /// Assets for imgsListview
+  /// assets/imgs/Listview.svg
+  static const String imgsListview = "assets/imgs/Listview.svg";
+
+  /// Assets for imgsLocation
+  /// assets/imgs/Location.svg
+  static const String imgsLocation = "assets/imgs/Location.svg";
+
+  /// Assets for imgsLogo
+  /// assets/imgs/Logo.svg
+  static const String imgsLogo = "assets/imgs/Logo.svg";
+
+  /// Assets for imgsMainImg
+  /// assets/imgs/main_img.png
+  static const String imgsMainImg = "assets/imgs/main_img.png";
+
+  /// Assets for imgsMenu
+  /// assets/imgs/Menu.svg
+  static const String imgsMenu = "assets/imgs/Menu.svg";
+
+  /// Assets for imgsPlus
+  /// assets/imgs/Plus.svg
+  static const String imgsPlus = "assets/imgs/Plus.svg";
+
+  /// Assets for imgsRing
+  /// assets/imgs/ring.png
+  static const String imgsRing = "assets/imgs/ring.png";
+
+  /// Assets for imgsSearch
+  /// assets/imgs/Search.svg
+  static const String imgsSearch = "assets/imgs/Search.svg";
+
+  /// Assets for imgsShipping
+  /// assets/imgs/Shipping.svg
+  static const String imgsShipping = "assets/imgs/Shipping.svg";
+
+  /// Assets for imgsShopping bag
+  /// assets/imgs/shopping bag.svg
+  static const String imgsShoppingbag = "assets/imgs/shopping bag.svg";
+
+  /// Assets for imgsStar
+  /// assets/imgs/star.png
+  static const String imgsStar = "assets/imgs/star.png";
+
+  /// Assets for imgsTag
+  /// assets/imgs/Tag.svg
+  static const String imgsTag = "assets/imgs/Tag.svg";
+
+  /// Assets for imgsTwitter
+  /// assets/imgs/twitter.svg
+  static const String imgsTwitter = "assets/imgs/twitter.svg";
+
+  /// Assets for imgsUp
+  /// assets/imgs/Up.svg
+  static const String imgsUp = "assets/imgs/Up.svg";
+
+  /// Assets for imgsVector
+  /// assets/imgs/Vector.svg
+  static const String imgsVector = "assets/imgs/Vector.svg";
+
+  /// Assets for imgsVoucher
+  /// assets/imgs/Voucher.svg
+  static const String imgsVoucher = "assets/imgs/Voucher.svg";
+
+  /// Assets for imgsYouMayAlsoLike1
+  /// assets/imgs/you_may_also_like_1.png
+  static const String imgsYouMayAlsoLike1 = "assets/imgs/you_may_also_like_1.png";
+
+  /// Assets for imgsYouMayAlsoLike2
+  /// assets/imgs/you_may_also_like_2.png
+  static const String imgsYouMayAlsoLike2 = "assets/imgs/you_may_also_like_2.png";
+
+  /// Assets for imgsYouTube
+  /// assets/imgs/YouTube.svg
+  static const String imgsYouTube = "assets/imgs/YouTube.svg";
+}
+
