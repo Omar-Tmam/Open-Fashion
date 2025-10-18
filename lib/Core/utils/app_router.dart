9417ce2bf0df57c67c33b18420eb0040dcbd1cbc
omartmam <omartmam9@gmail.com>
2025-10-18 15:38:09 +0300

    add product details to app router

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,15 +1,18 @@
 import 'package:go_router/go_router.dart';
 import 'package:open_fashion/Features/home_view/presentation/views/home_view.dart';
+import 'package:open_fashion/Features/product_details/presentation/views/product_details.dart';
 import 'package:open_fashion/Features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kSplashView = '/';
   static const kHomeView = '/homeView';
+  static const kProductDetailsView = '/productDetailsView';
 
   static final router = GoRouter(
     routes: [
       GoRoute(path: kSplashView, builder: (context, state) => SplashView()),
       GoRoute(path: kHomeView, builder: (context, state) => HomeView()),
+      GoRoute(path: kProductDetailsView, builder: (context, state) => ProductDetails()),
     ],
   );
 }
